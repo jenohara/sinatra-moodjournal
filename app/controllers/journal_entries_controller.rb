cfcf762be41069af4e3b4b75b7cbcef5d43ecf8d
LadyWitch <jenny.s.ohara@gmail.com>
2019-11-08 12:32:50 +0000

    add loging to post route

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,32 +1,45 @@
 class JournalEntriesController < ApplicationController
 
-  # GET: /journal_entries
-  get "/journal_entries" do
-    erb :"/journal_entries/index.html"
-  end
-
   # GET: /journal_entries/new
   get "/journal_entries/new" do
     erb :"/journal_entries/new.html"
   end
 
   # POST: /journal_entries
-  post "/journal_entries" do
-    redirect "/journal_entries"
+  post "/journal_entries" do 
+    if !logged_in?
+      redirect to "/"
+    end
+    if params[:content] != ""
+    @journal_entry = JournalEntry.create(date: params[:date], happy: params[:happy], bored: params[:bored], angry: params[:angry], excited: params[:excited], tired: params[:tired], frustrated: params[:frustrated], calm: params[:calm], sad: params[:sad], hurting: params[:hurting], content: params[:content], user_id: current_user.id)
+    redirect "/journal_entries/#{@journal_entry.id}"
+    
+    else
+      redirect to '/journal_entries/new'
+    end
   end
 
+   # GET: /journal_entries index route for all entries
+   get "/journal_entries" do
+    @journal_entries = JournalEntry.find
+    erb :"/journal_entries/index.html"
+  end
+  
   # GET: /journal_entries/5
   get "/journal_entries/:id" do
+    @journal_entry = JournalEntry.find(params[:id])
     erb :"/journal_entries/show.html"
   end
 
   # GET: /journal_entries/5/edit
   get "/journal_entries/:id/edit" do
+    @journal_entry = JournalEntry.find(params[:id])
     erb :"/journal_entries/edit.html"
   end
 
   # PATCH: /journal_entries/5
   patch "/journal_entries/:id" do
+    @journal_entry = JournalEntry.find(params[:id])
     redirect "/journal_entries/:id"
   end
 
@@ -34,4 +47,22 @@ class JournalEntriesController < ApplicationController
   delete "/journal_entries/:id/delete" do
     redirect "/journal_entries"
   end
+
+  helpers do
+  
+    def moods
+      @moods = []
+      @journal_entry = JournalEntry.find(params[:id])
+
+      @journal_entry.all.each do |element|
+        if element != nil && element != date && element != content
+          @moods << element
+          binding.pry
+          @moods
+        end
+      end
+    end
+  end
+
+
 end
